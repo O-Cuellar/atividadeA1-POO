9775f51ccc93fe03fa12e2230639c6f136f9b0ab
O-Cuellar <cuellarotavio0@gmail.com>
2025-03-17 18:25:58 -0400

    ver 0.3

--- src/questoes/Questao03.java
+++ src/questoes/Questao03.java
@@ -1,5 +1,20 @@
 package questoes;
 
+import java.util.Scanner;
+
 public class Questao03 extends BaseQuestao{
+    Scanner scanner = new Scanner(System.in);
     
+    public double Converte(double celsius){
+        double fahrenheit = celsius * 1.8 + 32;
+        return fahrenheit;
+    }
+
+    public void Executar(){
+        System.out.println("\n--------CONVERSOR DE TEMPERATURA--------\n");
+        System.out.println("Digite a temperatura em Celsius: ");
+        double celsius = scanner.nextDouble();
+        double fahrenheit = Converte(celsius);
+        System.out.println("A temperatura em Fahrenheit é: " + fahrenheit);
+    }   
 }
