c01f8567c6586f5b1b24ddb6a84e4dbaa925bed3
O-Cuellar <cuellarotavio0@gmail.com>
2025-03-20 01:52:04 -0400

    ver 0.4

--- src/questoes/Questao04.java
+++ src/questoes/Questao04.java
@@ -1,5 +1,27 @@
 package questoes;
 
+import java.util.Scanner;
+
 public class Questao04 extends BaseQuestao{
-    
+    Scanner in = new Scanner(System.in);
+
+    public void Verifica(int alunos, int alunas){
+        if(alunos == alunas){
+            System.out.println("O número de alunos é igual ao número de alunas.");
+        } else if(alunos > alunas){
+            System.out.println("O número de alunos é maior que o número de alunas.");
+        } else{
+            System.out.println("O número de alunos é menor que o número de alunas.");
+        }
+
+    }
+
+    public void Executar(){
+        System.out.println("\n-----Verifica se o número de alunos é igual ao número de alunas-----");
+        System.out.println("Digite o número de alunos: ");
+        int alunos = in.nextInt();
+        System.out.println("Digite o número de alunas: ");
+        int alunas = in.nextInt();
+        Verifica(alunos, alunas);
+    }
 }
