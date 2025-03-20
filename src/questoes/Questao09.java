8be401feea7fb5cb7719110b9fe8b39b65a73d22
O-Cuellar <cuellarotavio0@gmail.com>
2025-03-20 17:55:56 -0400

    ver 0.9

--- src/questoes/Questao09.java
+++ src/questoes/Questao09.java
@@ -6,6 +6,8 @@ public class Questao09 extends BaseQuestao{
     Scanner scanner = new Scanner(System.in);
     
     public void Executar(){
-
+        System.out.println("Digite o valor inicial da conta de água: ");
+        double valorInicial = scanner.nextInt();
+        
     }
 }
--- src/questoes/Questao10.java
+++ src/questoes/Questao10.java
@@ -5,7 +5,31 @@ import java.util.Scanner;
 public class Questao10 extends BaseQuestao{
     Scanner scanner = new Scanner(System.in);
 
-    public void Executar(){
+    public double ImpostoBruto(double salarioMinimo, double salarioFuncionario){
+        double impostoBruto = 0;
+        double taxa = 0;
+        double impostoLiquido = 0;
+        if (salarioFuncionario >= salarioMinimo * 12){
+            impostoBruto = 0.2;
+            } else if (salarioFuncionario >= salarioMinimo * 5){
+                impostoBruto = 0.08;
+                } else {
+                    impostoBruto = 0;
+                }
+        taxa = impostoBruto * 0.04;
+        impostoLiquido = impostoBruto + taxa;
+        return impostoLiquido;
+    }
 
+    public void Executar(){
+        System.out.println("Digite o valor do salário-mínimo: ");
+        double salarioMinimo = scanner.nextDouble();
+        System.out.println("Digite o número de dependentes: ");
+        int numDependentes = scanner.nextInt();
+        System.out.println("Digite o valor do salário do funcionário: ");
+        double salarioFuncionario = scanner.nextDouble();
+        System.out.println("Digite o valor do imposto já pago pelo funcionario: \n ");
+        double impostoAnterior = scanner.nextDouble();
+        double impostoBruto = ImpostoBruto(salarioMinimo,  salarioFuncionario);
     }
 }
