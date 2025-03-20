d878471ba874f7733f3c59351f1606fc44c214be
O-Cuellar <cuellarotavio0@gmail.com>
2025-03-20 14:16:58 -0400

    ver 0.6.9

--- src/questoes/Questao07.java
+++ src/questoes/Questao07.java
@@ -5,17 +5,29 @@ import java.util.Scanner;
 public class Questao07 extends BaseQuestao{
     Scanner scanner = new Scanner(System.in);
     
-    public void Pagamento(float totalGasto){
+    public void Pagamento(double totalGasto){
+        double totalPago;
+        
         System.out.println("Digite a opção de pagamento:\n");
         System.out.println("1 - A vista com 10% de desconto\n");
         System.out.println("2 - Em duas vezes (preço da etiqueta)\n");
         System.out.println("3 - de três até 10 vezes com 3% de juros ao mês (somente para compras acima de R$ 100,00\n");
         int option = scanner.nextInt();
+        switch (option) {
+            case 1:
+                totalPago = totalGasto - (totalGasto * 0.1);
+                System.out.println("Opção de pagamento número 1 escolhida, a vista com 10% de desconto, valor a ser pago: " + totalPago);
+            case 2:
+                totalPago = totalGasto/2;
+                System.out.println("Opção de pagamento número 2 escolhida, em duas vezes (preço de etiqueta), valor a ser pago em cada parcela: " + totalPago);
+            case 3:
+        }
     }
 
     public void Executar(){
-        System.out.println("Digite o total gasto: ");
-        float totalGasto = scanner.nextFloat();
+        System.out.println("\n----Calculo de pagamento em compras da loja de roupas----");
+        System.out.println("\nDigite o total gasto: ");
+        double totalGasto = scanner.nextDouble();
         Pagamento(totalGasto);
     }
 }
